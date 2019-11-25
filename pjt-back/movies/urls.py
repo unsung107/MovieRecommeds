471695306738491291a455cefc7f180f89c31983
unsung107 <unsung102@naver.com>
2019-11-25 13:33:18 +0900

    making home

--- pjt-back/movies/urls.py
+++ pjt-back/movies/urls.py
@@ -6,4 +6,6 @@ app_name = 'movies'
 urlpatterns = [
     path('movieupdate/', views.movieupdate, name='movieupdate'),
     path('commingmovieupdate/', views.commingmovieupdate, name='commingmovieupdate'),
+    path('api/v1/<int:genre_id>/', views.homemovielist, name='homemovielist'),
+    path('api/v1/searchNm/<str:movie_nm>/', views.searchMovie, name='searchMovie'),
 ]
--- pjt-front/src/views/Home.vue
+++ pjt-front/src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="getMovie(1)">범죄</button>
+    <button @click="getMovie(2)">드라마</button>
+    <button @click="getMovie(3)">액션</button>
+    <button @click="getMovie(5)">SF</button>
+    <button @click="getMovie(11)">판타지</button>
+    <br>
+
+    <input type="text" v-model="searchKey" @input="searching(searchKey)">
+    
+    <div v-for="movie in movieList" :key="movie.id">{{movie.title}}</div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data() {
+    return {
+      movies: [],
+      searchKey: '',
+    };
+  },
+  components: {},
+  methods: {
+    getMovie(genre_id) {
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP;
+      axios
+        .get(SERVER_IP + `/movies/api/v1/${genre_id}/`)
+        .then(response => {
+          console.log(response.data);
+          this.movies = response.data.movies
+          console.log(this.movies)
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+
+    searching(movie_nm) {
+      if (!movie_nm) {
+        movie_nm = ' '
+      }
+      const SERVER_IP = process.env.VUE_APP_SERVER_IP;
+      axios
+        .get(SERVER_IP + `/movies/api/v1/searchNm/${movie_nm}/`)
+        .then(response => {
+          this.movies = response.data.movies
+          
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  mounted() {
+    
+  },
+  computed: {
+    movieList() {
+      return this.movies
+    }
   }
-}
+};
 </script>
