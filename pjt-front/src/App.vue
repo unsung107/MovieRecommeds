cbf8aa55d46a5da8e51720dde2a02e74d8f4c388
unsung107 <unsung102@naver.com>
2019-11-25 09:55:03 +0900

    ready for login

--- pjt-front/src/App.vue
+++ pjt-front/src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="app">
-    <button @click="movieUpdate">실험</button>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  
-  name: 'app',
-  components: {
-  },
-  methods: {
-    movieUpdate() {
-      axios.get('http://127.0.0.1:8000/movies/commingmovieupdate/')
-      .then(response => {
-        console.log(response)
-      })
-      
-    }
-  }
-
-}
-</script>
 
 <style>
 #app {
@@ -32,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
